063176285bcf80516ebc32c00a7297e7222617e4
Sagar Atalatti <sagaratalatti@users.noreply.github.com>
2024-11-08 14:31:02 +0530

    Initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    '@babel/plugin-transform-class-properties',
+    '@babel/plugin-transform-nullish-coalescing-operator',
+    '@babel/plugin-transform-optional-chaining',
+  ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,23 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  const { resolver: { sourceExts, assetExts } } = defaultConfig;
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  return {
+    ...defaultConfig,
+    resolver: {
+      ...defaultConfig.resolver,
+      sourceExts: [...sourceExts, 'cjs'],
+      assetExts: [...assetExts],
+      extraNodeModules: {
+        ...require('node-libs-react-native'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        url: require.resolve('url'),
+        https: require.resolve('https-browserify'),
+        http: require.resolve('stream-http'),
+      },
+    },
+  };
+})();
